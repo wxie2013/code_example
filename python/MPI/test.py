daf64545e770935c8e044615d13b56b2fce35fa0
wxie2013 <wxie@cern.ch>
2021-10-08 18:18:05 -0400

    use MPI to do sum

--- python/MPI/test.py
+++ python/MPI/test.py
@@ -1,7 +1,26 @@
+import numpy as np
 from mpi4py import MPI
+import code_example.python.repeated_K_fold as rkf
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-print("Hello world from rank", str(rank), "of", str(size))
+print('train_index@rank', rank, ':', rkf.list_train_index[rank])
+print('test_index@rank', rank, ':', rkf.list_test_index[rank])
+
+sum_train = np.zeros(1)
+sum_test = np.zeros(1)
+sum_train[0] = sum(rkf.list_train_index[rank])
+sum_test[0] = sum(rkf.list_test_index[rank])
+print('sum_train@rank', rank, ': ', sum_train[0]) 
+print('sum_test@rank', rank, ': ', sum_test[0])
+
+##
+total_sum_train = np.zeros(1)
+total_sum_test = np.zeros(1)
+comm.Reduce(sum_train, total_sum_train, op=MPI.SUM, root=0)
+comm.Reduce(sum_test, total_sum_test, op=MPI.SUM, root=0)
+
+if rank == 0:
+    print('total_sum_train: ', total_sum_train, ' total_sum_test: ', total_sum_test)
--- python/repeated_K_fold.py
+++ python/repeated_K_fold.py
@@ -1,17 +1,20 @@
 import numpy as np
-from sklearn.model_selection import KFold, RepeatedKFold
-X = np.array([[1, 2], [3, 4], [6, 7], [8, 9]])
-y = np.array([1, 2, 3, 4])
-#kf = KFold(n_splits=2)
-kf = RepeatedKFold(n_splits=2, n_repeats=2, random_state=2652124)
+from sklearn.model_selection import RepeatedKFold
+
+X = np.array([]);
+for i in range(10):
+    X = np.append(X, i);
+
+#
+num_splits = 4
+num_repeat = 2
+
+#
+kf = RepeatedKFold(n_splits=num_splits, n_repeats=num_repeat, random_state=2652124)
 kf.get_n_splits(X)
-print(kf)
+
+list_train_index = []
+list_test_index = []
 for train_index, test_index in kf.split(X):
-     print("TRAIN:", train_index, "TEST:", test_index)
-     X_train, X_test = X[train_index], X[test_index]
-     y_train, y_test = y[train_index], y[test_index]
-     print('X_train: ', X_train)
-     print('X_test: ', X_test)
-     print('y_train: ', y_train)
-     print('y_test: ', y_test)
-     print('--------------------------')
+     list_train_index.append(train_index)
+     list_test_index.append(test_index)
