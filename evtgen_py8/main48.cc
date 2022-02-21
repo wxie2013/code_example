9411db4538d36c10e06b4edc76c43ddd2edc7709
Wei Xie <wei.xie@cern.ch>
2022-02-21 11:14:47 -0500

    evtgen and pythia example is working now

--- evtgen_py8/main48.cc
+++ evtgen_py8/main48.cc
@@ -67,6 +67,7 @@ int main(int argc, char* argv[]) {
   if (use) {
     setenv("PYTHIA8DATA", argv[3], 1);
     evtgen = new EvtGenDecays(&pythia, argv[1], argv[2]);
+    //evtgen->readDecayFile(argv[1]);
     evtgen->readDecayFile("main48.dec");
   }
 
@@ -112,6 +113,6 @@ int main(int argc, char* argv[]) {
   pythia.stat();
   mass /= mass.getEntries();
   cout << "!!!!!: mass: "<<mass<<endl;
-  if (evtgen) delete evtgen;
+  //if (evtgen) delete evtgen;
   return 0;
 }
