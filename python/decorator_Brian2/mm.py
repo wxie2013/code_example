6305755beb4d6a8ce1a6b6ad590a0225fe119093
Wei Xie <wei.xie@cern.ch>
2022-09-28 13:15:23 -0400

    mm.py spike_rate.cpp

--- python/decorator_Brian2/mm.py
+++ python/decorator_Brian2/mm.py
@@ -1,32 +1,30 @@
 import os
 from brian2 import *
 
-def read_spike_rate_code(spike_mon_obj):
+current_dir = os.path.dirname(__file__)
+ta2d = TimedArray([[1, 2, 3, 4, 5, 6], [100, 200, 300, 400, 500, 600]], dt=0.2*ms)
+
+# read the cpp code 
+def read_spike_rate_code():
     with open(os.path.join(current_dir,'spike_rate.cpp')) as f:
         spike_rate_code = f.read()
 
-    return spike_rate_code.replace('%%SPIKECOUNT%%',
-        device.get_array_name(spikemon.variables['count']))
+    return spike_rate_code
 
-
-
-current_dir = os.path.dirname(__file__)
-set_device('cpp_standalone', directory='standalone', debug=True)
-
-ta2d = TimedArray([[1, 2, 3, 4, 5, 6], [100, 200, 300, 400, 500, 600]], dt=0.2*ms)
-
-N = 6
-input_neurons = NeuronGroup(N, '''rate : Hz
-                                  scale: 1''', threshold='rand()<rate*dt')
-spikemon = SpikeMonitor(input_neurons)
-
-spike_rate_code = read_spike_rate_code(spikemon)
-@implementation('cpp', spike_rate_code,
+# defined user function 
+@implementation('cpp', read_spike_rate_code(),
                 dependencies={'ta2d': ta2d})
 @check_units(t=second, i=1, result=1)
 def spike_rate(scale, t, i):
     raise NotImplementedError('use standalone mode')
 
+#
+set_device('cpp_standalone', directory='standalone', debug=True)
+
+N = 6
+input_neurons = NeuronGroup(N, '''rate : Hz
+                                  scale: 1''', threshold='rand()<rate*dt')
+spikemon = SpikeMonitor(input_neurons, name='ttt')
 input_neurons.scale = [0.1, 1, 10, 100, 1000, 10000]
 input_neurons.run_regularly('rate=spike_rate(scale, t, i)*Hz', dt=0.2*ms)
 run(0.3*ms, report='text')
--- python/decorator_Brian2/spike_rate.cpp
+++ python/decorator_Brian2/spike_rate.cpp
@@ -1,7 +1,7 @@
 // spike_rate.cpp
 double spike_rate(double scale, double t, int i) {
    std::cout<<"t = " << t << "s" <<std::endl;
-   std::cout<<"spike count for neuron " << i << ": " <<brian::%%SPIKECOUNT%%[i]<<std::endl;
+   std::cout<<"spike count for neuron " << i << ": " <<brian::_array_ttt_count[i]<<std::endl;
    return ta2d(t, i)*scale;
 }
 
