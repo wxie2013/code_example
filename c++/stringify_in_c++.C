9e21b3e293af7324e8b0527106a65ef9c942c408
Wei Xie <wei.xie@cern.ch>
2022-09-28 13:22:00 -0400

    stringify_in_c++.C

--- c++/stringify_in_c++.C
+++ c++/stringify_in_c++.C
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <cstring>
 #define get_name(name) #name
 
 using namespace std;
 int main() {
   int aa = 10;
   string bb = get_name(aa);
+  const char *cc = get_name(aa);
   if(bb=="aa")
-    cout<<bb<<endl;
+    cout<<"bb: "<<bb<<endl;
   else
     cout<<"!!!! no such name !!!"<<endl;
 
+  if(strcmp(cc, "aa")==0)
+    cout<<"cc: "<<bb<<endl;
   return 0;
 }
