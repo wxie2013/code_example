feaa148cf1bf688a25d26dafeabe1087d4f96e32
Wei Xie <wei.xie@cern.ch>
2023-05-02 10:28:06 -0400

    find_if_with_pair.C

---
+++ c++/find_if_with_pair.C
@@ -0,0 +1,23 @@
+{
+    vector<pair<int,float>> a;
+    vector<pair<int,float>> b;
+
+    a.push_back(make_pair(1, 10));
+    a.push_back(make_pair(2, 20));
+    a.push_back(make_pair(3, 30));
+    a.push_back(make_pair(4, 40));
+    a.push_back(make_pair(5, 50));
+
+    b.push_back(make_pair(1, 100));
+    b.push_back(make_pair(2, 200));
+    b.push_back(make_pair(3, 300));
+    b.push_back(make_pair(4, 400));
+    b.push_back(make_pair(5, 500));
+
+    for(int i = 0; i<a.size(); i++) {
+        auto c = a[i];
+        auto it = find_if(b.begin(), b.end(), [&c](pair<int, float> &elem) {return elem.first==c.first;});
+        cout<< "a[i].first: "<<a[i].first<<" a[i].second: "<<a[i].second<<" b.first: "<<it->first<<" b.second: "<<it->second<<endl;
+
+    }
+}
