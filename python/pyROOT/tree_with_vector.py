a54309dc6f3a10b61a4ed82ae280b3e5ec15aa46
Wei <wxavier2013@gmail.com>
2024-08-22 16:30:19 -0400

    tree_with_vector.py

--- python/pyROOT/tree_with_vector.py
+++ python/pyROOT/tree_with_vector.py
@@ -3,19 +3,20 @@ from ROOT import TFile, TTree, addressof, gROOT
 gROOT.ProcessLine(
 "struct data_t {\
    int  m;\
-   Char_t  n[5];\
+   std::string  *n;\
    std::vector<int> *E;\
 };")
 
 data = ROOT.data_t()
-data.E = ROOT.std.vector(int)() # define it here 
+data.E = ROOT.vector[int]() # define it here 
+data.n = ROOT.string();
 
 t1 = TTree("t1", "EMC Photons");
 t1.Branch("m", addressof(data, 'm'), 'm/I');
 t1.Branch("E", data.E);
-t1.Branch("n", addressof(data, 'n'), "n/C");
+t1.Branch("n", data.n);
 
-data.n = "1234";
+data.n.assign("hello");
 data.m = 10;
 for i in range(data.m):
     data.E.push_back(i);
