6e795a7be220810b77911cba35e14dab10c0a52e
Wei Xie <wei.xie@cern.ch>
2024-08-23 10:58:55 -0400

    tree_with_vector.py

--- python/pyROOT/tree_with_vector.py
+++ python/pyROOT/tree_with_vector.py
@@ -7,23 +7,27 @@ gROOT.ProcessLine(
    std::vector<int> *E;\
 };")
 
-data = ROOT.data_t()
-data.E = ROOT.vector[int]() # define it here 
-data.n = ROOT.string();
+def make_tree():
+    data.E = ROOT.vector[int]() # define it here 
+    data.n = ROOT.string();
+    
+    t1 = TTree("t1", "EMC Photons");
+    t1.Branch("m", addressof(data, 'm'), 'm/I');
+    t1.Branch("E", data.E);
+    t1.Branch("n", data.n);
+    
+    data.n.assign("hello");
+    data.m = 10;
+    for i in range(data.m):
+        data.E.push_back(i);
+    
+    t1.Fill()
+    t1.Scan("E:m:n")
 
-t1 = TTree("t1", "EMC Photons");
-t1.Branch("m", addressof(data, 'm'), 'm/I');
-t1.Branch("E", data.E);
-t1.Branch("n", data.n);
+    return t1
 
-data.n.assign("hello");
-data.m = 10;
-for i in range(data.m):
-    data.E.push_back(i);
-
-t1.Fill()
-t1.Print()
-t1.Scan("E:m:n")
-
-for entry in t1:
-    print(entry.n, entry.m, entry.E)
+if __name__ == '__main__':
+    data = ROOT.data_t()
+    t1 = make_tree()
+    for entry in t1:
+        print(entry.n, entry.m, entry.E)
