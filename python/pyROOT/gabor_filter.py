9d5d35a87867ccbdf17bdb273cd9f69519398223
wxie2013 <wxie@purdue.edu>
2025-01-29 09:54:55 -0500

    gabor_filter.py

--- python/pyROOT/gabor_filter.py
+++ python/pyROOT/gabor_filter.py
@@ -2,17 +2,18 @@ import ROOT
 from ROOT import TF2, TCanvas
 
 theta = 0.0
-lam = 2.0
+lam =10 
 gamma = 0.5
 psi = 0 
-sigma =0.785
+sigma = 30
 
 xp = f"x*cos({theta})+y*sin({theta})"
 yp = f"-x*sin({theta})+y*cos({theta})"
 fun_gf_real = f"exp(-(({xp})**2+{gamma}**2*({yp})**2)/(2*{sigma}**2))*cos(6.28*({xp})/{lam} + {psi})"
 
 print(fun_gf_real)
-f_real = TF2("f_real", fun_gf_real, -10, 10, -10, 10)
+f_real = TF2("f_real", fun_gf_real, -200, 200, -200, 200)
+f_real.SetNpx(2000)  # need to set this manually to avoid truncated plotting issues
 c0 = TCanvas()
 f_real.Draw("surf2")
 
