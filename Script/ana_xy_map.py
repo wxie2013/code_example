d0a323a50a3fae9f6784df9fcc0c3a342a6c2a97
wxie <wei.xie@cern.ch>
2026-05-21 11:55:39 -0400

    ana_xy_map.py

--- Script/ana_xy_map.py
+++ Script/ana_xy_map.py
@@ -1,32 +1,63 @@
 import ROOT
 from Brian2.tools.general_tools import *
 
+project_dir = "/home/wxie/Brian2/model"
+dir_neuron_in = f"{project_dir}/neuron_group/neurons_in"
+dir_neuron_FE = f"{project_dir}/neuron_group/neurons_FE0"
+dir_in2fe0 = f"{project_dir}/syn_out/epoch_0/seg_0_10/in2fe0"
+
 f = ROOT.TFile('result.root', 'RECREATE')
-nt = ROOT.TNtuple('nt', 'nt', 'xe:ye:xi:yi')
-nte = ROOT.TNtuple('nte', 'nt', 'x:y')
+nt = ROOT.TNtuple('nt', 'nt', 'xi:yi:xe:ye:theta:order')
+nte = ROOT.TNtuple('nte', 'nt', 'x:y:theta:order')
 nti = ROOT.TNtuple('nti', 'nt', 'x:y')
-neuron = read_data('neuron', 0)
-syn = read_data('S_e2S_input2c', 0)
-input2c = read_data('S_input2c', 0)
 
-xi = []
-yi = []
-xe = neuron['x']
-ye = neuron['y']
+neuron_in = read_data(dir_neuron_in, 0)
+neuron_FE = read_data(dir_neuron_FE, 0)
+S_in2fe0 = read_data(dir_in2fe0, 0)
 
-for i in range(784):
-    xi.append(i%28)
-    yi.append(28-i//28)
-    nti.Fill(xi[i], yi[i])
+xi = neuron_in['x']
+yi = neuron_in['y']
+xe = neuron_FE['x']
+ye = neuron_FE['y']
+theta_RF = neuron_FE['theta_RF']
+order_RF = neuron_FE['order_RF']
 
-for x, y in zip(xe, ye):
-    nte.Fill(x, y)
+for x, y in zip(xi, yi):
+    nti.Fill(x, y)
 
-pre_id = input2c['i']
+for x, y, t, o in zip(xe, ye, theta_RF, order_RF):
+    nte.Fill(x, y, t, o)
 
-for i, j in zip(syn['i'], syn['j']):
-    ii = pre_id[j]
-    nt.Fill(xe[i], ye[i], xi[ii], yi[ii])
+for i, j in zip(S_in2fe0['i'], S_in2fe0['j']):
+    nt.Fill(xi[i], yi[i], xe[j], ye[j], theta_RF[j], order_RF[j])
+
+cc = ROOT.TCanvas()
+cc.cd()
+nte.SetMarkerStyle(20)
+nte.SetMarkerSize(2)
+nt.SetMarkerStyle(24)
+nti.Draw("y:x")
+nte.Draw("y:x", "", "same")
+
+for x, y in set(zip(xe, ye)):
+    for xt, yt, t in set(zip(xe, ye, theta_RF)):
+        if x != xt or y != yt:
+            continue
+        for j,t1 in enumerate(list(set(theta_RF))):
+            #nti.Draw("y:x")
+            #nte.Draw("y:x", "", "same")
+            if t != t1:
+                continue 
+            for i, o in enumerate(list(set(order_RF))):
+                nt.SetMarkerColor(i%5+j%5+1)
+                condition = f"abs(theta -{t})<0.1 && order == {o} && abs(ye-{y})<0.1 && abs(xe-{x})<0.1"
+                nt.Draw("yi:xi", condition, "same")
+                cc.Modified()
+                cc.Update()
+                input(f"x: {x}, y: {y}, theta: {t}, order: {o}")
+            #cc.Clear()
+
+ROOT.gDirectory.Clear()
 
 f.cd()
 nt.Write()
